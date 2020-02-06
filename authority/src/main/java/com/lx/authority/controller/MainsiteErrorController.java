745bebcb55d322953f73a7371ed8614dd9d081d5
52ylx <1024006972@qq.com>
2020-02-06 09:18:27 +0800

    myWeb

--- authority/src/main/java/com/lx/authority/controller/MainsiteErrorController.java
+++ authority/src/main/java/com/lx/authority/controller/MainsiteErrorController.java
@@ -2,10 +2,13 @@ package com.lx.authority.controller;//说明:
 
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 /**[2019/7/3]说明: 如果页面不存在返回错误页
    __ _ _  __    ___   __    __   __ _ _
@@ -14,25 +17,34 @@ import javax.servlet.http.HttpServletRequest;
  \ \ \ \     [##] |#|___/#/\#\     / / / /
   \_\_\_\    [##] |####/#/  \#\   /_/_/*/
 @Controller
-public class MainsiteErrorController implements ErrorController {
+public class MainsiteErrorController implements ErrorController,EnvironmentAware {
+
+    private String e404="error/404";
+    private String e500="error/500";
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request){
         //获取statusCode:401,404,500
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         if(statusCode == 401){
-            return "error/404";
+            return e404;
         }else if(statusCode == 404){
-            return "error/404";
+            return e404;
         }else if(statusCode == 403){
-            return "error/404";
+            return e404;
         }else{
-            return "error/500";
+            return e500;
         }
 
     }
     @Override
     public String getErrorPath() {
         return "/error";
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.e404 = Optional.ofNullable(environment.getProperty("server.e404")).orElse("error/404");
+        this.e500 = Optional.ofNullable(environment.getProperty("server.e500")).orElse("error/500");
     }
 }
